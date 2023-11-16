0784423275996ce43da6cad977fb7583b74f811b
Teddy Burns <code@teddyburns.com>
2023-11-15 21:27:41 -0800

    Use map to look up route redirects

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 #[macro_use] extern crate rocket;
 
 use rocket::response::{status::NotFound, Redirect};
+use std::{sync::Mutex, collections::HashMap};
+use once_cell::sync::Lazy;
+
+static mut ROUTES: Lazy<Mutex<HashMap<&str, &'static str>>> = Lazy::new(|| {
+    let mut routes: HashMap<&str,  &'static str> = HashMap::new();
+    routes.insert("git", "https://github.com");
+    Mutex::new(routes)
+});
 
 
 #[get("/redirect/<path>")]
 fn index(path: &str) -> Result<Redirect, NotFound<String>>{
-    if path != "git" {
-        return Err(NotFound(format!("Golink for {} not found", path)));
+    let routes = unsafe { & *ROUTES.lock().unwrap() };
+    match routes.get(path) {
+        Some(value) => {
+            return Ok(Redirect::to(*value));
+        },
+        None => return Err(NotFound(format!("Golink for {} not found", path))),
     }
-
-    return Ok(Redirect::to(uri!("https://github.com")));
 }
 
 #[launch]
